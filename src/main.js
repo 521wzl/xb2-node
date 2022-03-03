4e08750424ebe0e66fdcc9739037ab1c41522a50
wangali <wzl289771808@gmail.com>
2022-03-03 19:37:16 +0800

    带参数的应用接口

--- src/main.js
+++ src/main.js
@@ -1,11 +1,38 @@
-// 用express创建一个应用接口
-
+// 定义带参数应用接口
 const express= require('express');
 const port=3000;
 const app=express();
-app.get('/',(request,response)=>{// get()方法有两个参数，第一个参数是地址
-    response.send('hello');
+data = [
+    {
+        'id': 1,
+        'title': '关山月',
+        'content': '明月出天山,苍茫云海间'
+
+    },
+    {
+        'id': 2,
+        'title': '望岳',
+        'content': '会当凌绝顶,一览众山小'
+
+    },
+    {
+        'id': 3,
+        'title': '忆江南',
+        'content': '日出江花红似火,春来江水绿如蓝'
+
+    }
+
+];
+
+app.get('/host/:postID',(request,response)=>{
+    console.log(request.params);
+    const {postID}=request.params;
+    const posts=data.filter(item=>item.id==postID);
+    response.send(posts[0]);
+
+
 });
-app.listen(port,()=>{// 只提供一个端口
+app.listen(port,()=>{
     console.log('服务已启动');
-});
+})
+
