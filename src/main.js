59dc5b18aa3bea071235d7cabb147880e1dcaf27
wangali <wzl289771808@gmail.com>
2022-03-03 10:00:15 +0800

    响应json格式文件

--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
 const http=require('http');
-
+const data ={
+        'id': 1,
+        'title': 'music',
+        'content': 'a-star'
+    
+    };
+const jsondata=JSON.stringify(data);// 必须进行格式转换，将文本格式转换成json格式
 const server=http.createServer((request,response)=>{
-    switch(request.url){// switch循环必须带{}
-        case '/':// 一种情况胡必须带‘：’
-        response.write('hello');
-        break;// 一种情况结束，必须用break终止本次循环.否则会一直输出下面内容
-        case '/hosts':
-        response.write('welcome');
-        break;
-        case '/posts':
-        response.write('love');
-        break;
-        default:
-        response.writeHead(404);
-        response.write('404');// 404作为响应内容，只能是字符串形式，终端会报错，出现无法连接页面
-        break;
-    }
-    response.end();// 这个很重要，没有这句客户端就会卡住不动
+    response.writeHead(200,{
+        'content-type': 'application/json; charset=utf-8'//声明类型为json，编码形式为utf-8
+    });
+    response.write(jsondata)
+    response.end();
 
 });
-server.listen(3000);// 必须监听端口，否则request.url就不知道父亲是谁,出现无法连接页面
+server.listen(3000);
