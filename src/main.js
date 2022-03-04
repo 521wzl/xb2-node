1abce76021eb3009c268a3c556d9b019dbf4c2f9
wangali <wzl289771808@gmail.com>
2022-03-04 09:39:04 +0800

    定义一个创建内容资源应用的接口

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
-// 用express创建一个应用接口
+// 定义一个创建内容资源的接口
 
 const express= require('express');
 const port=3000;
 const app=express();
-app.get('/',(request,response)=>{// get()方法有两个参数，第一个参数是地址
-    response.send('hello');
+app.use(express.json());
+app.post('/posts',(request,response)=>{
+    const {content}=request.body;
+    response.status(201);//可以自己创建一个响应状体码
+    console.log(request.header('sing-along'));// 将在客户端设置的头部信息输出到控制台
+    response.set('sing-along','what you want?');// 对请求里的头部信息在响应头部信息里作出响应
+    response.send({
+        'message': `成功创建内容:${content}`
+    });
 });
 app.listen(port,()=>{// 只提供一个端口
     console.log('服务已启动');
